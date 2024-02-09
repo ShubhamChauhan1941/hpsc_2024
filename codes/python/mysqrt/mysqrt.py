0cc8614e38f98b59540b6e3cb4802af697243651
ShubhamChauhan1941 <golu.chouhan.@gmail.com>
2024-02-09 12:47:51 +0530

    Converted to sqrt2(x) function

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,16 +1,13 @@
-x=float(input("enter value you want sqrt"))
-s=2.0
-kmax=100
-tolerance=1.0e-14 
-for k in range (kmax):
-    k=k+1
-    print(f"At iteration {k} the value is = {s}")
-    s0=s
-    s=0.5*(s+(x/s))
-    delta_s=s-s0
-    if (abs(delta_s)/x<tolerance):
-        break
-
-	
-	
-print(f"finally at iteration {k} the value is = {s}")
+def sqrt2(x):
+	s=2.0
+	kmax=100
+	tolerance=1.0e-14 
+	for k in range (kmax):
+		k=k+1
+		print(f"At iteration {k} the value is = {s:20.15f}")
+		s0=s
+		s=0.5*(s+(x/s))
+		delta_s=s-s0
+		if (abs(delta_s)/x<tolerance):
+			break
+	print(f"finally at iteration {k} the value is = {s:20.15f}")
