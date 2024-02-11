fbf283388bfb3d9c614238d85c86f9cdf77e6061
ShubhamChauhan1941 <golu.chouhan.@gmail.com>
2024-02-11 20:15:22 +0530

    convergent error has been resolved

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -5,9 +5,15 @@ Module for printing the square root of fuction by newton Raphson Method
 
 #this is the sqrt command
 def sqrt2(x,debug):
+	from numpy import nan
 	"""
 	sqrt function 
 	"""
+	if x==0:
+		return 0
+	elif x<0:
+		return nan
+		
 
 	s=2.0
 	kmax=100
@@ -17,9 +23,10 @@ def sqrt2(x,debug):
 		if debug:
 			print(f"At iteration {k} the value is = {s:20.15f}")
 		s0=s
-		s=0.5*(s+(x/s))
+		s= 0.5*(s+(x/s))
+		#for cube root s=0.3333*((2*s)+(x/(s**2)))
 		delta_s=s-s0
-		if (abs(delta_s)/x<tolerance):
+		if (abs(delta_s/x)<tolerance):
 			break
 	if debug:
 		print(f"finally at iteration {k} the value is = {s:20.15f}")
