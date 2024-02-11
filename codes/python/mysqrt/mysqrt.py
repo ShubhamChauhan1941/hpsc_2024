6a5013bcc3db18e17cd5eae9a834a286fc3e9211
ShubhamChauhan1941 <golu.chouhan.@gmail.com>
2024-02-11 11:26:42 +0530

    added a debug flag and return value

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -4,7 +4,7 @@ Module for printing the square root of fuction by newton Raphson Method
 """
 
 #this is the sqrt command
-def sqrt2(x):
+def sqrt2(x,debug):
 	"""
 	sqrt function 
 	"""
@@ -14,10 +14,13 @@ def sqrt2(x):
 	tolerance=1.0e-14 
 	for k in range (kmax):
 		k=k+1
-		print(f"At iteration {k} the value is = {s:20.15f}")
+		if debug:
+			print(f"At iteration {k} the value is = {s:20.15f}")
 		s0=s
 		s=0.5*(s+(x/s))
 		delta_s=s-s0
 		if (abs(delta_s)/x<tolerance):
 			break
-	print(f"finally at iteration {k} the value is = {s:20.15f}")
+	if debug:
+		print(f"finally at iteration {k} the value is = {s:20.15f}")
+	return s
